b6a91984f6fa1f01c3181e319806c5136133b58d
luisf <luisfernandod.teixeira@gmail.com>
2025-05-08 11:07:36 -0300

    Iniciando a aplicação

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+  ],
+  exports: [MatToolbarModule],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
